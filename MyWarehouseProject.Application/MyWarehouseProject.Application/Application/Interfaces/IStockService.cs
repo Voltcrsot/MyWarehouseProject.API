367972ca8f2fe72d35d489a77f94bba8d60808d9
Voltcrsot <131707223+Voltcrsot@users.noreply.github.com>
2024-09-14 20:41:56 +0300

    Add files via upload

---
+++ MyWarehouseProject.Application/MyWarehouseProject.Application/Application/Interfaces/IStockService.cs
@@ -0,0 +1,14 @@
+﻿using MyWarehouseProject.Application.DTOs;
+using System;
+using System.Threading.Tasks;
+
+namespace MyWarehouseProject.Application.Interfaces
+{
+    public interface IStockService
+    {
+        Task<StockDto> GetStockByIdAsync(Guid id);
+      
+        Task AddStockAsync(StockDto stockDto);
+        Task UpdateStockAsync(StockDto stockDto);
+    }
+}
